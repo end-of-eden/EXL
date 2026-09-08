3124f046f2f54b3b8b97981fe418dec621640869
end-of-eden <seonghunsim05@gmail.com>
2026-09-09 02:23:52 +0900

    Remove repository photo entries from the gallery

--- js/archive-shell.js
+++ js/archive-shell.js
@@ -71,7 +71,7 @@
       selectRoute(route, historyMode);
     });
     var contentUrl = new URL(route.url.href);
-    if (route.path === 'gallery.html') contentUrl.searchParams.set('v', '20260909-sidebar');
+    if (route.path === 'gallery.html') contentUrl.searchParams.set('v', '20260909-upload-only');
     next.src = contentUrl.href;
     slot.appendChild(next);
     return true;
