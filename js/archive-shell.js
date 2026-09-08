f3fc98fe1331acaabe9118f897236e6daa29e926
end-of-eden <seonghunsim05@gmail.com>
2026-09-09 02:20:25 +0900

    Move Butterscotch below banner and replace broken X font icon

--- js/archive-shell.js
+++ js/archive-shell.js
@@ -71,7 +71,7 @@
       selectRoute(route, historyMode);
     });
     var contentUrl = new URL(route.url.href);
-    if (route.path === 'gallery.html') contentUrl.searchParams.set('v', '20260909-hagu');
+    if (route.path === 'gallery.html') contentUrl.searchParams.set('v', '20260909-sidebar');
     next.src = contentUrl.href;
     slot.appendChild(next);
     return true;
