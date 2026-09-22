2e3be98d6ece1593848dcd8291d19451fcd287a4
end-of-eden <busyhyundai@gmail.com>
2026-09-22 17:31:40 +0900

    Use silver sparkle background in light mode

--- js/archive-shell.js
+++ js/archive-shell.js
@@ -79,9 +79,9 @@
       selectRoute(route, historyMode);
     });
     var contentUrl = new URL(route.url.href);
-    if (route.path === 'agents.html' || route.path === 'wiki.html') contentUrl.searchParams.set('v', '20260922-water-soft');
-    if (route.path === 'gallery.html') contentUrl.searchParams.set('v', '20260922-water-soft');
-    if (route.path === 'log.html' || route.path.startsWith('Log/')) contentUrl.searchParams.set('v', '20260922-water-soft');
+    if (route.path === 'agents.html' || route.path === 'wiki.html') contentUrl.searchParams.set('v', '20260922-silver');
+    if (route.path === 'gallery.html') contentUrl.searchParams.set('v', '20260922-silver');
+    if (route.path === 'log.html' || route.path.startsWith('Log/')) contentUrl.searchParams.set('v', '20260922-silver');
     next.src = contentUrl.href;
     slot.appendChild(next);
     return true;
