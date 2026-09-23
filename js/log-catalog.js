416e1c40dd3d883c133d0ed0c069f2a979cdc156
end-of-eden <seonghunsim05@gmail.com>
2026-09-23 22:37:20 +0900

    Change strawberry court log music

--- js/log-catalog.js
+++ js/log-catalog.js
@@ -16,7 +16,7 @@ window.ARCHIVE_LOCAL_LOGS = [
     date: '2026.09.23',
     bodyPath: 'Log/text/arch-006.txt?v=20260923-character-fix',
     ooc: 'OOC: 새로운 에피소드. NPC는 꿈속에서 재판을 받는다. 재판관, 검사, 변호사, 서기, 방청객까지 재판정 안의 모든 인물은 작은 SD 사이즈의 PC이다. 엄숙해야 할 재판정은 어딘가 엉성하고 귀엽고, 절차도 묘하게 핀트가 어긋나 있지만 꿈속의 NPC는 이를 전혀 이상하게 여기지 못한다. 재판에서는 NPC의 죄목을 상정한다. 검사는 NPC의 혐의를 낭독하고, 변호사는 변호하는 듯하면서도 NPC에게 도움이 되기보다 실책을 하고만다. NPC는 나름대로 자기 변호를 시도한다. 최종적으로 NPC는 꿈 속의 모든 PC를 안아주는 형벌을 받게된다. 꿈 속 재판 장면을 묘사하고, NPC가 꿈에서 깨어나 어떤 감정을 느끼는지 자세히 서술한다.',
-    music: { videoId: 'K4xLi8IF1FM', title: '딸기밭의 법정 BGM' },
+    music: { videoId: 'TXzfQ0cP1P0', title: '딸기밭의 법정 BGM' },
     glass: true,
     plainDialogue: true,
     highlights: ['재물 손괴 및 동심 파괴', '심각한 서운함', '피고인의 뇌 구조', '모든 시아를 불안하게 만든 죄', '십 초 이상 꽉 안아줄 것', '저를 안아달라는 어리광'],
